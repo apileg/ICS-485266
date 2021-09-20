2714aba2d2500a95ce34a5ec453f4c7b3b24df59
Oleg Shapoval <mr.gamestyle1@gmail.com>
2021-09-20 12:53:51 +0300

    created project.py

---
+++ Lab3/Scripts/project.py
@@ -0,0 +1,28 @@
+import random
+
+deck = [6,7,8,9,10,2,3,4,11] * 4
+count = 0
+
+random.shuffle(deck);
+
+print('Поиграем в очко?')
+
+while True:
+    choice = input('Будете брать карту? y/n\n')
+    if choice == 'y':
+        current = deck.pop()
+        print('Вам попалась карта достоинством %d' %current)
+        count += current
+        if count > 21:
+            print('Извините, но вы проиграли')
+            break
+        elif count == 21:
+            print('Поздравляю, вы набрали 21!')
+            break
+        else:
+            print('У вас %d очков.' %count)
+    elif choice == 'n':
+        print('У вас %d очков и вы закончили игру.' %count)
+        break
+
+print('До новых встреч!')
