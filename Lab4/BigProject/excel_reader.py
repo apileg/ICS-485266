4b29ff63adf2952397afce09bed0a04d6cf40293
Oleg Shapoval <mr.gamestyle1@gmail.com>
2021-09-28 12:34:49 +0300

    add: bigproject

---
+++ Lab4/BigProject/excel_reader.py
@@ -0,0 +1,35 @@
+# Import the necessary packages
+import pandas as pd
+from excel_opener import open_file
+
+# Read excel file from open_file function
+data = pd.read_excel(fr'{open_file()}')
+
+
+def read_file():
+    try:
+        # Make table data structure
+        df = pd.DataFrame(data)
+        print(df)
+    except FileNotFoundError:
+        print("Oops! I can't find the file.")
+
+
+def read_file_column():
+    print(f"Available columns: {get_all_columns()}\n")
+    # Variables
+    column = input("Enter name of column: ")
+
+    if column in get_all_columns():
+        # Make table data structure
+        df = pd.DataFrame(data, columns=[column])
+        print(df)
+    else:
+        print("Oops! I can't find the column.")
+
+
+def get_all_columns():
+    """
+    :return: String
+    """
+    return data.columns.values
---
+++ Lab4/BigProject/main.py
@@ -0,0 +1,36 @@
+# Took the console menu from https://pypi.org/project/console-menu/
+# Import the necessary packages
+from consolemenu import *
+from consolemenu.items import *
+
+# Import variables and functions from root files
+import excel_opener
+from excel_reader import *
+
+# Variables
+column = get_all_columns()
+
+
+# Functions TODO: change string "File path.." to real file path
+def console_menu_subtitle(subtitle: str):
+    return ConsoleMenu("Laboratory work", subtitle)
+
+
+# Create the menu
+menu = ConsoleMenu("Laboratory work", excel_opener.get_file_path())
+
+# A FunctionItem runs a Python function when selected
+# TODO: To prevent the window from closing; now workaround
+print_excel_table = FunctionItem("Print excel table", read_file, args=None, kwargs=None, menu=ConsoleMenu,
+                                 should_exit=False)
+
+print_excel_table_column = FunctionItem("Print excel column", read_file_column, args=None, kwargs=None,
+                                        menu=ConsoleMenu, should_exit=False)
+
+
+# Once we're done creating them, we just add the items to the menu
+menu.append_item(print_excel_table)
+menu.append_item(print_excel_table_column)
+
+# Finally, we call show to show the menu and allow the user to interact
+menu.show()
