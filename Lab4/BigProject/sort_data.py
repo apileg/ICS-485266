351ef487332376788fa0f9d9a4c609084cb4ccb2
Oleg Shapoval <mr.gamestyle1@gmail.com>
2021-10-18 13:25:03 +0300

    refactor: sort

--- Lab4/BigProject/sort_data.py
+++ Lab4/BigProject/sort_data.py
@@ -20,19 +20,33 @@ def sort_data():
     # standardizing our df type to dictionary
     df_dict = df.to_dict()
     result_arr = {}
+    newarr = {}
 
     for i in df_dict:
+        try:
+            # ar[i][j] - Try to add to the dictionary keys with values
+            result_arr[i].append("")
+        except KeyError:
+            # ar[i][j] - In case of failure create keys with values for the dictionary
+            result_arr[i] = []
         # Column - i
         if i == column:
             # Column - i, Value - j
             for j in df_dict[i]:
                 # Hashing data for comparison
-                if hashlib.md5(str(df_dict[i][j]).encode()).hexdigest() == hashlib.md5(str(value).encode()).hexdigest():
-                    try:
-                        # ar[i][j] - Try to add to the dictionary keys with values
-                        result_arr[i].append(df_dict[i][j])
-                    except KeyError:
-                        # ar[i][j] - In case of failure create keys with values for the dictionary
-                        result_arr[i] = [df_dict[i][j]]
+                result_arr[i].append(df_dict[i])
+                if hashlib.md5(str(df_dict[i][j]).encode()).hexdigest() != hashlib.md5(str(value).encode()).hexdigest():
+                    newarr = remove_from_array(df_dict, j)
+
+    for x in newarr.copy():
+        for y in newarr[x].copy():
+            if (newarr[x][y] == None):
+                newarr[x].pop(y)
     # Print DataFrame with our columns and rows
-    print(pd.DataFrame(result_arr))
+    print(pd.DataFrame(newarr))
+
+
+def remove_from_array(dictionary, idx):
+    for i in dictionary:
+        dictionary[i][idx] = None
+    return dictionary
