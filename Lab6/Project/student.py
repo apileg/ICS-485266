bf40a2bf9c5fd63fdbb9dcfeb1832331631fab44
Oleg Shapoval <mr.gamestyle1@gmail.com>
2021-11-10 22:07:19 +0200

    initial commit

---
+++ Lab6/Project/student.py
@@ -0,0 +1,18 @@
+class Student:
+    grade = {
+        'max_exam_grade': 100,
+        'max_laboratory_grade': 5,
+        'number_of_laboratory_works': 10,
+        'scores_for_automatic_evaluation': 0.6,
+    }
+
+    def __init__(self, name: str, grade: float):
+        self.name = name
+        self.grade = grade
+
+    def exam_attempts(self, attempts: int, last_grade: float):
+        print(f'Attempts: {attempts}\n Last grade:{last_grade}')
+
+    def individual_attempts(self, attempts: int, last_grade: float):
+        print(f'Attempts: {attempts}\n Last grade:{last_grade}')
+
