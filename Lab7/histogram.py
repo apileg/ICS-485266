02576f54eac35cfde78e2156b4a025db58433899
Oleg Shapoval <mr.gamestyle1@gmail.com>
2021-11-25 18:06:08 +0200

    initial commit

--- Lab7/histogram.py
+++ Lab7/histogram.py
@@ -1,8 +1,11 @@
 from collections import Counter
+
+import pylab
 from matplotlib import pyplot as plt
 import numpy as np
 
-def create_histogram(text: str):
+
+def create_text_histogram(text: str):
     counts = Counter(text.replace(" ", ""))
     for i in text:
         plt.bar(i, counts[i])
@@ -10,5 +13,12 @@ def create_histogram(text: str):
     plt.show()
 
 
+def create_sentence_histogram(sentence: str):
+    symbols = [':', ',', '.', '!', '?']
+    for i in range(0, len(symbols)):
+        pylab.bar(symbols[i], sentence.count(symbols[i]))
+    pylab.show()
 
-create_histogram("ssomme ttexxt")
+
+create_text_histogram("ssomme ttexxt")
+create_sentence_histogram("Hello, world!")
